8925d683f87af984beeb5e3f7660438edab8c916
Kushal Khanal <kushalkhanal777@gmail.com>
2026-07-27 00:04:04 +0545

    update blog views

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+from django.db.models import F
 
-# Create your views here.
-
-from django.http import HttpResponse
-
+from .models import BlogPost
 
 
 def blog(request):
-    return HttpResponse("Hi! This is blog page")
+    """Display all published blog posts."""
+    posts = BlogPost.objects.filter(is_published=True)
+    paginator = Paginator(posts, 6)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+    return render(request, 'blog/blog.html', {'posts': posts})
+
+
+def blog_detail(request, slug):
+    """Display a single blog post."""
+    post = get_object_or_404(BlogPost, slug=slug, is_published=True)
+    BlogPost.objects.filter(id=post.id).update(views_count=F('views_count') + 1)
+    related = BlogPost.objects.filter(is_published=True, category=post.category).exclude(id=post.id)[:3]
+    return render(request, 'blog/blog_detail.html', {'post': post, 'related': related})
