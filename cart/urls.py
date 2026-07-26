7440dda6f0facb735db44375a3bf0501f924ab02
Kushal Khanal <kushalkhanal777@gmail.com>
2026-07-27 00:01:55 +0545

    add cart urls

---
+++ cart/urls.py
@@ -0,0 +1,12 @@
+"""Cart URL configuration."""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.cart_detail, name='cart_detail'),
+    path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('update/<int:item_id>/', views.update_cart, name='update_cart'),
+    path('remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('coupon/apply/', views.apply_coupon, name='apply_coupon'),
+    path('coupon/remove/', views.remove_coupon, name='remove_coupon'),
+]
